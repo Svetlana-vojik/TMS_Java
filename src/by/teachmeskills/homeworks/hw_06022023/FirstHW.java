8b5915d0a99a8b43da53c0b78cc930ee0aa4402f
svetlana.ezheleva <sourire06@mail.ru>
2023-02-06 15:13:27 +0300

    Initial changes

--- src/by/teachmeskills/homeworks/hw_06022023/FirstHW.java
+++ src/by/teachmeskills/homeworks/hw_06022023/FirstHW.java
@@ -4,7 +4,7 @@ public class FirstHW {
     public static void main(String[] args) {
         int a = 5;
         int b = 4;
-        int sum = a+b;
-        System.out.println("Sum: "+ sum);
+        int minus = a-b;
+        System.out.println(minus);
     }
 }
