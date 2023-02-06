2be242203924115d86a3ec044b90e3eba4426e10
Svetlana Ezheleva <svetlana.ezheleva@minsk.mm.local>
2023-02-06 14:25:05 +0300

    Initial commit

---
+++ src/by/teachmeskills/homeworks/hw_06022023/FirstHW.java
@@ -0,0 +1,10 @@
+package by.teachmeskills.homeworks.hw_06022023;
+
+public class FirstHW {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int sum = a+b;
+        System.out.println("Sum: "+ sum);
+    }
+}
