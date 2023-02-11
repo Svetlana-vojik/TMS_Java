8a2feb9c7a0112ff129dd23730189169db4f757b
svetlana.ezheleva <sourire06@mail.ru>
2023-02-11 13:06:04 +0300

    solved

---
+++ src/by/teachmeskills/homeworks/hw_17022023/DaysOfTheWeek.java
@@ -0,0 +1,22 @@
+package by.teachmeskills.homeworks.hw_17022023;
+
+import java.util.Scanner;
+
+public class DaysOfTheWeek {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       int day = scan.nextInt();
+//      int day = 5;
+
+        switch (day) {
+            case 1 -> System.out.println("Понедельник");
+            case 2 -> System.out.println("Вторник");
+            case 3 -> System.out.println("Среда");
+            case 4 -> System.out.println("Четверг");
+            case 5 -> System.out.println("Пятница");
+            case 6 -> System.out.println("Суббота");
+            case 7 -> System.out.println("Воскресенье");
+            default -> throw new IllegalArgumentException("Число не соответствует дню недели");
+        }
+    }
+}
