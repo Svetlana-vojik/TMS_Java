a3dbf40ab9b7831ae8eb8cde9e1cdf8aa7b1ab7d
svetlana.ezheleva <sourire06@mail.ru>
2023-02-12 11:15:32 +0300

    solved

---
+++ src/by/teachmeskills/homeworks/hw_17022023/Stars.java
@@ -0,0 +1,10 @@
+package by.teachmeskills.homeworks.hw_17022023;
+
+public class Stars {
+    public static void main(String[] args) {
+        for (int i=1; i <= 4; i++) {
+            System.out.println("*****");
+        }
+
+    }
+}
