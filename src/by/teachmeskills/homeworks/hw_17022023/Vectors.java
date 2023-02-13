cd2c16c0e23133e17ddca01183ccdbfcb2c4674c
svetlana.ezheleva <sourire06@mail.ru>
2023-02-13 21:31:18 +0300

    solved

--- src/by/teachmeskills/homeworks/hw_17022023/Vectors.java
+++ src/by/teachmeskills/homeworks/hw_17022023/Vectors.java
@@ -2,15 +2,33 @@ package by.teachmeskills.homeworks.hw_17022023;
 
 public class Vectors {
     public static void main(String[] args) {
-        int [] array = new int []{ 2, -5, 10, -8, 4, 12, -4, 5, -6};
-            int sum = 0;
+        int[] array = {2, -5, 10, 7, -4, 12};
+        int sum = 0;
+        int max = 0;
+        int min = 0;
+        int multiply = 1;
 
-            for(int i = 0; i  < array.length; i++){
-                if (array[i] < 0){
-                sum = sum + array[i];
-                }
+      for (int i = 0; i < array.length; i++) {
+           if (array[i] < 0) {
+              sum = sum + array[i];
+           }
+       }
+       System.out.println("Сумма отрицательных элементов: " + sum);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > array[max]) {
+                max = i;
             }
-        System.out.println(sum);
+            if (array[i] < array[min]) {
+                min = i;
+            }
+        }for ( int i = min + 1; i <= max - 1; i++){
+            int temp = array[i];
+            multiply = multiply * array[i];
+        }
+        System.out.println("Произведение: " + multiply);
+
+        System.out.println("Max: " + array[min] + "; " + "Min: " + array[max]);
     }
 }
 
