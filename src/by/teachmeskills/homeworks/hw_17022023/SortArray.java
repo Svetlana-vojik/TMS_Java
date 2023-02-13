c2056576f8e8582a5e753f6cc6c72ba6d9b8c3a2
svetlana.ezheleva <sourire06@mail.ru>
2023-02-13 17:30:44 +0300

    solved

---
+++ src/by/teachmeskills/homeworks/hw_17022023/SortArray.java
@@ -0,0 +1,29 @@
+package by.teachmeskills.homeworks.hw_17022023;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SortArray {
+    public static void main(String[] args) {
+
+        int [] array = new int [10];
+        boolean sorted = false;
+        int temp;
+
+        for (int i = 0; i < array.length; i++ ){
+            array[i] = (int) (Math.random() * 20);
+        }
+            while (!sorted){
+                sorted = true;
+                for (int i = 0; i < array.length - 1; i++){
+                    if (array[i] > array[i + 1]){
+                        temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        sorted = false;                    }
+                }
+            }System.out.println(Arrays.toString(array));
+        }
+
+
+        }
