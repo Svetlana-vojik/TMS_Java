d16c928d6400c3fc820f17206e8fc752aed1f49b
svetlana.ezheleva <sourire06@mail.ru>
2023-02-14 19:20:47 +0300

    changes in code

--- src/by/teachmeskills/homeworks/hw_17022023/Doublearray2.java
+++ src/by/teachmeskills/homeworks/hw_17022023/Doublearray2.java
@@ -2,17 +2,18 @@ package by.teachmeskills.homeworks.hw_17022023;
 
 public class Doublearray2 {
     public static void main(String[] args) {
-        int[][] num = new int[2][];
+        int[][] num = new int[10][];
 
 
         for (int i = 0; i < num.length; i++) {
-            num[i] = new int[i + 1];
+            num[i] = new int[(int) (Math.random() * 20)];
             for (int j = 0; j < num[i].length; j++) {
                 num[i][j] = j + 1;
                 System.out.print(num[i][j]);
+
             }
+            System.out.println();
         }
-        System.out.println();
+
     }
 }
-//int) (Math.random() * 20)
