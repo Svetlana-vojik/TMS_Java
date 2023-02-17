884d1ac302bf163ab82f7a8148c2f946bfc2165b
svetlana.ezheleva <sourire06@mail.ru>
2023-02-17 12:57:32 +0300

    solved

---
+++ src/by/teachmeskills/homeworks/hw_24022023/trainings.java
@@ -0,0 +1,15 @@
+package by.teachmeskills.homeworks.hw_24022023;
+
+public class trainings {
+    public static void main(String[] args) {
+        int way = 10;
+        int way7 = way;
+
+        System.out.println("Пробег за 1: " + way);
+        for (int i = 2; i <= 7; i++) {
+            way += way / 10;
+            way7 += way;
+            System.out.println("Пробег за " + i + ": " + way);
+        }
+    }
+}
