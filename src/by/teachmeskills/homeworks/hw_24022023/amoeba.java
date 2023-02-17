d9b30e2874c2a15cbb957e6eb0f780f1c0d1c6d5
svetlana.ezheleva <sourire06@mail.ru>
2023-02-17 13:28:49 +0300

    solved

---
+++ src/by/teachmeskills/homeworks/hw_24022023/amoeba.java
@@ -0,0 +1,16 @@
+package by.teachmeskills.homeworks.hw_24022023;
+
+public class amoeba {
+    public static void main(String[] args) {
+        int a = 1;
+        int time = 24;
+
+        while (time >= 3) {
+            for (int i = 0; i < time; i++) {
+                a *= 2;
+                time -= 3;
+                System.out.println(a);
+            }
+        }
+    }
+}
