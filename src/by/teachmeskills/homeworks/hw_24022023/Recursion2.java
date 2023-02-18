cb7936219e660c81f906f393dab9404758f34883
svetlana.ezheleva <sourire06@mail.ru>
2023-02-18 11:56:56 +0300

    small changes

--- src/by/teachmeskills/homeworks/hw_24022023/Recursion2.java
+++ src/by/teachmeskills/homeworks/hw_24022023/Recursion2.java
@@ -7,16 +7,12 @@ public class Recursion2 {
     }
 
     public static String recursion(int a, int b) {
-        if (a > b) {
-            if (a == b) {
-                return Integer.toString(a);
-            }
+        if (a == b) {
+            return Integer.toString(a);
+        } else if (a > b) {
             return a + " " + recursion(a - 1, b);
-        } else {
-            if (a == b) {
-                return Integer.toString(a);
-            }
-            return a + " " + recursion(a + 1, b);
         }
+        return a + " " + recursion(a + 1, b);
+
     }
 }
