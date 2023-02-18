bd9cd5149d60a814724078c947e5d988985a3633
svetlana.ezheleva <sourire06@mail.ru>
2023-02-18 11:30:37 +0300

    solved

---
+++ src/by/teachmeskills/homeworks/hw_24022023/Recursion1.java
@@ -0,0 +1,19 @@
+package by.teachmeskills.homeworks.hw_24022023;
+
+//public class Recursion1 {
+//    public static int main(String[] args) {
+//      System.out.println();
+//        //int[] nums = {1, 5, 6, 12, 18, 20, 32, 1, 8, 4};
+
+    //}
+
+//    private static int sum (int[] array, int i) {
+//        if (i != array.length) {
+//            sum(array, ++i);
+//            return sum(array, ++i);
+//        }
+//
+//        return i;
+//
+//    }
+//}
