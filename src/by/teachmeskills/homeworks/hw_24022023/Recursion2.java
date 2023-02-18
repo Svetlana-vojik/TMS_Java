1aefe7ad7d760fff1bb7c110c21cc0a1017a6209
svetlana.ezheleva <sourire06@mail.ru>
2023-02-18 11:57:19 +0300

    small changes

--- src/by/teachmeskills/homeworks/hw_24022023/Recursion2.java
+++ src/by/teachmeskills/homeworks/hw_24022023/Recursion2.java
@@ -3,9 +3,8 @@ package by.teachmeskills.homeworks.hw_24022023;
 public class Recursion2 {
     public static void main(String[] args) {
         System.out.println(recursion(41, 50));
-        System.out.println(recursion(-10, -5));
+        System.out.println(recursion(-10,-5));
     }
-
     public static String recursion(int a, int b) {
         if (a == b) {
             return Integer.toString(a);
@@ -13,6 +12,5 @@ public class Recursion2 {
             return a + " " + recursion(a - 1, b);
         }
         return a + " " + recursion(a + 1, b);
-
     }
 }
