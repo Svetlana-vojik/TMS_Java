a766de1d8ab3d97787bfd59feb84964f442fad92
svetlana.ezheleva <sourire06@mail.ru>
2023-02-18 11:31:24 +0300

    solved

---
+++ src/by/teachmeskills/homeworks/hw_24022023/Recursion3.java
@@ -0,0 +1,22 @@
+package by.teachmeskills.homeworks.hw_24022023;
+
+public class Recursion3 {
+    public static void main(String[] args) {
+        double n = 16;
+        if (recursion(n) == 1) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+
+        }
+    }
+    public static int recursion(double n) {
+        if (n == 1) {
+            return 1;
+        } else if (n > 1 && n < 2) {
+            return 0;
+        } else {
+            return recursion(n / 2);
+        }
+    }
+}
