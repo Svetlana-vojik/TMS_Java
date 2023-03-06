26635211688b7c99171e1b0dd1df6559eee06dc2
svetlana.ezheleva <sourire06@mail.ru>
2023-03-06 19:12:39 +0300

    small changes

--- src/by/teachmeskills/homeworks/hw_03032023/transport/Transport.java
+++ src/by/teachmeskills/homeworks/hw_03032023/transport/Transport.java
@@ -1,6 +1,6 @@
 package by.teachmeskills.homeworks.hw_03032023.transport;
 
-public class Transport {
+public abstract class Transport {
     protected int power;
     protected int maxSpeed;
     protected int weight;
@@ -13,11 +13,43 @@ public class Transport {
         this.brand = brand;
     }
 
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     protected double horsePowerToKW() {
         return power * 0.74;
     }
 
-       public String toString() {
+    public String toString() {
         return "мощность = " + power +
                 " лошадиная сила или " + horsePowerToKW() + " кВ, макисмальная скорость = " + maxSpeed +
                 " км/ч, масса = " + weight + " тонн, марка = " + brand + ", ";
