aa603d8eefdce62344794fd227a09a4a1a92be95
svetlana.ezheleva <sourire06@mail.ru>
2023-03-28 17:17:54 +0300

    solved

--- src/by/teachmeskills/homeworks/hw_17032023/CheckingFile.java
+++
@@ -1,4 +0,0 @@
-package by.teachmeskills.homeworks.hw_17032023;
-
-public class CheckingFile {
-}
---
+++ src/by/teachmeskills/homeworks/hw_17032023/censorship/Main.java
@@ -0,0 +1,37 @@
+package by.teachmeskills.homeworks.hw_17032023.censorship;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            String[] text = new String(Files.readAllBytes(Paths.get("src\\by\\teachmeskills\\homeworks\\hw_17032023\\files\\censor.txt"))).split("\\.");
+            List<String> blackWords = Files.readAllLines(Paths.get("src\\by\\teachmeskills\\homeworks\\hw_17032023\\files\\black_list.txt"));
+            List<String> wrongSentences = new ArrayList<>();
+            for (String blackWord : blackWords) {
+                for (String sentence : text) {
+                    if (sentence.toLowerCase().contains(blackWord.toLowerCase()) &&
+                            !wrongSentences.contains(sentence)) {
+                        wrongSentences.add(sentence);
+                    }
+                }
+            }
+            if (wrongSentences.size() == 0) {
+                System.out.println("Файл прошел проверку на цензуру");
+            } else {
+                System.out.println("Количество предложений не прошедших цензуру " + wrongSentences.size());
+                System.out.println();
+                for (String wrongSentence : wrongSentences) {
+                    System.out.println("ПРЕДЛОЖЕНИЕ | " + wrongSentence.trim());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
