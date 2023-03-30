655e28b8eacffc250a6e42030ff037d5039c1a85
svetlana.ezheleva <sourire06@mail.ru>
2023-03-30 12:18:39 +0300

    solved

--- src/by/teachmeskills/homeworks/hw_17032023/serialization/Main.java
+++ src/by/teachmeskills/homeworks/hw_17032023/serialization/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car("Audi", 180, 50000);
         try {
-            FileOutputStream fos = new FileOutputStream("src\\by\\teachmeskills\\homeworks\\hw_17032023\\files\\car.txt");
+            FileOutputStream fos = new FileOutputStream("src\\by\\teachmeskills\\homeworks\\hw_17032023\\files\\car.dat");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
 
             oos.writeObject(car);
@@ -16,7 +16,7 @@ public class Main {
             throw new RuntimeException(e);
         }
         try {
-            FileInputStream fis = new FileInputStream("src\\by\\teachmeskills\\homeworks\\hw_17032023\\files\\car.txt");
+            FileInputStream fis = new FileInputStream("src\\by\\teachmeskills\\homeworks\\hw_17032023\\files\\car.dat");
             ObjectInputStream ois = new ObjectInputStream(fis);
 
             Car car1 = (Car) ois.readObject();
