c32cb127945e09538129b422d6e7f0a473948c3b
svetlana.ezheleva <sourire06@mail.ru>
2023-04-01 15:30:38 +0300

    solved

---
+++ src/by/teachmeskills/homeworks/hw_31032023/generics/Main.java
@@ -0,0 +1,10 @@
+package by.teachmeskills.homeworks.hw_31032023.generics;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sum is " + CalculatorUtil.sum(5.8, 2));
+        System.out.println("Difference is " + CalculatorUtil.difference(10, 5));
+        System.out.println("Divide is " + CalculatorUtil.divide(12, 2.5));
+        System.out.println("Multiply is " + CalculatorUtil.multiply(2, 2));
+    }
+}
